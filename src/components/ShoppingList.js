d28a84161f5b906d6cd3652cf86479f281301013
Arnaud <arnaud.joguet@gmail.com>
2022-12-15 10:19:12 +0100

    chore(ShoppingList): add some comments

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,6 +1,7 @@
 import React  from 'react';
 import { plantList } from '../datas/plantList';
 
+// first, add data from array
 /* const planList = [
     'monstera',
     'ficus lyrata',
@@ -9,13 +10,18 @@ import { plantList } from '../datas/plantList';
     'palmier'
 ]; */
 
+// add datas from another file with name, category and id for each plant
+
 function ShoppingList() {
+    // create categeries const from planlist file
     const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
 		[]
 	)
-
+    
+    // foreach show name for each categories
+    // and show name for each plant and add id in key.
 	return (
 		<div>
 			<ul>
